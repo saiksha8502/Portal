f4bf0e9c18d57a200b3afc00ff0f23bb824706e5
Saiksha Desai <104218478+saiksha8502@users.noreply.github.com>
2023-01-26 16:15:32 +0530

    Update Login.aspx.cs

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -8,73 +8,28 @@ using System.Data;
 using System.Web.UI.WebControls.WebParts;
 using System.Data.OracleClient;
 using System.Data.OleDb;
-
-
-
-
 public partial class Login : System.Web.UI.Page
 {
     string strConnString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
-
     OracleCommand com;
     OracleDataAdapter orada;
-
     string str;
     DataTable dt;
     int RowCount;
     protected void Page_Load(object sender, EventArgs e)
     {
-        //fillgrid();
+       
     }
-    //protected void Login_Click(object sender, EventArgs e)
-    //{
-
-    //    OracleConnection con = new OracleConnection(strConnString);
-    //    con.Open();
-
-    //    str = "Select * from signup where username='" + usernameTXT.Text + "' and newpassword='" + PasswordTXT.Text + "' ";
-    //         com = new OracleCommand(str);
-    //        orada = new OracleDataAdapter(com.CommandText, con);
-    //        dt = new DataTable();
-    //        orada.Fill(dt);
-    //        RowCount = dt.Rows.Count;
-    //        int flag = 0;
-    //        if (dt.Rows.Count == 0)
-    //        {
-    //            string query = "insert into login values('" + usernameTXT.Text + "','" + PasswordTXT.Text + "',sysdate)";
-    //            OracleCommand cmd = new OracleCommand(query, con);
-    //            cmd.ExecuteNonQuery();
-    //            con.Close();
-    //            Response.Redirect("default.aspx");
-    //            Label1.Visible = true;
-    //            Label1.Text = "login is Successfull";
-
-
-    //    }
-    //    else
-    //    {
-    //        //Response.Redirect("EPMasterPage.master");
-    //        Label1.Visible = true;
-    //        Label1.Text = "Invalid User Name or Password! Please try again! ";
-
-    //    }
-
-
-    //}
-    protected void Login_Click(object sender, EventArgs e)
-    {
-      
-
+     protected void Login_Click(object sender, EventArgs e)
+    {   
         OracleConnection con = new OracleConnection(strConnString);
         con.Open();
         str = "Select * from signup t where username='" + usernameTXT.Text + "' and newpassword='" + PasswordTXT.Text + "' ";
-       
         com = new OracleCommand(str);
         orada = new OracleDataAdapter(com.CommandText, con);
         dt = new DataTable();
         orada.Fill(dt);
         RowCount = dt.Rows.Count;
-
         if (RowCount==1)
         {
            // Response.Write(RowCount);
@@ -92,7 +47,6 @@ public partial class Login : System.Web.UI.Page
             Label1.Text = "Invalid User Name or Password! Please try again!";
         }
 
-
     }
     protected void FP_Click(object sender, EventArgs e)
     {
@@ -106,7 +60,7 @@ public partial class Login : System.Web.UI.Page
     {
         usernameTXT.Text = string.Empty;
         PasswordTXT.Text = string.Empty;
-        
+       
     }
-    
+  
 }
